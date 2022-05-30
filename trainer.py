589768bb70f6efea45be532cf43aec6b8aa80dc2
0x404 <871206929@qq.com>
2022-05-31 00:08:57 +0800

    feat: init checkpoint

--- trainer.py
+++ trainer.py
@@ -16,8 +16,10 @@ from torch.utils.tensorboard import SummaryWriter
 
 logger = get_logger(__name__)
 
+
 def collate_fn(batch):
     return batch
+
 
 label_image = [None for _ in range(6000)]
 input_with_ratio = {
@@ -28,6 +30,7 @@ input_with_ratio = {
 test_patch_x = 500
 test_patch_y = 500
 
+
 class Trainer:
     def __init__(self, config) -> None:
         self.config = config
@@ -35,10 +38,12 @@ class Trainer:
         self.model = UNet(config.model.channel_in, config.model.channel_out)
         self.model.to(self.device)
         self.saver = Saver(self.model, config)
+        if config.train.init_checkpoint is not None:
+            self.saver.resume_from_file(config.train.init_checkpoint)
         self.writer = None
         if config.setup.tensorboard:
             self.writer = SummaryWriter()
-    
+
     def load_data(self, item, config, transfroms=None, fixed=False):
         image_id = item["id"]
         ratio = item["ratio"]
@@ -51,7 +56,7 @@ class Trainer:
             raw = snoy_pack(raw)
             raw = np.expand_dims(raw, axis=0) * ratio
             input_with_ratio[str(ratio)[:3]][image_id] = raw
-        
+
         # load and cache label image
         if label_image[image_id] is None:
             image = rawpy.imread(str(label_path))
@@ -61,10 +66,10 @@ class Trainer:
                 half_size=config.raw_process.half_size,
                 output_bps=config.raw_process.output_bps,
             )
-            image = np.float32(image/65535.0)
+            image = np.float32(image / 65535.0)
             image = np.expand_dims(image, axis=0)
             label_image[image_id] = image
-        
+
         # apply patch
         if config.transform.use_patch:
             # the reason why we make label patch [3, patch_size * 2, patch_size * 2]
@@ -80,79 +85,81 @@ class Trainer:
             if fixed:
                 rand_x = 500
                 rand_y = 500
-            input = input_with_ratio[str(ratio)[:3]][image_id][:, rand_x : rand_x + patch_size, rand_y : rand_y + patch_size, :]
+            input = input_with_ratio[str(ratio)[:3]][image_id][
+                :, rand_x : rand_x + patch_size, rand_y : rand_y + patch_size, :
+            ]
             label = label_image[image_id][
                 :,
                 rand_x * 2 : rand_x * 2 + patch_size * 2,
                 rand_y * 2 : rand_y * 2 + patch_size * 2,
                 :,
             ]
-        
+
         # apply transfrom
         if transfroms is not None:
             for trans in transfroms:
                 input, label = trans(input, label, config.transform)
-                
+
         input = torch.from_numpy(input.copy())
         label = torch.from_numpy(label.copy())
         input = input.permute(0, 3, 1, 2).to(self.device)
         label = label.permute(0, 3, 1, 2).to(self.device)
         return input, label
-        
-        
-        
+
     def train(self):
         traincfg = self.config.train
         datacfg = self.config.data
         setup = self.config.setup
         loss_function = torch.nn.L1Loss()
         loss_function.to(self.device)
-        
+
         optimizer = optim.Adam(self.model.parameters(), lr=traincfg.lr)
         lr_scheduler = optim.lr_scheduler.StepLR(
             optimizer=optimizer,
             step_size=traincfg.lr_step,
             gamma=traincfg.lr_gamma,
         )
-        
+
         dataset = DataSetSID(
             config=datacfg,
             file=Path(datacfg.data_root).joinpath("Sony_train_list.txt").absolute(),
         )
         dataloader = DataLoader(dataset, collate_fn=collate_fn)
-        
+
         transforms = []
         if datacfg.transform.use_flip:
             transforms.append(filp_transform)
         if datacfg.transform.use_rotation:
             transforms.append(rotation_transform)
-        
+
         total_step = traincfg.max_step
         if total_step is None:
             total_step = traincfg.epochs * len(dataloader)
-        total_step = min(total_step, traincfg.epochs * len(dataloader))        
+        total_step = min(total_step, traincfg.epochs * len(dataloader))
         completed_step = 0
         for epoch in range(traincfg.epochs):
             for _, data in enumerate(dataloader):
                 input, label = self.load_data(data[0], datacfg, transforms)
                 output = self.model(input)
-                
+
                 loss = loss_function(output, label)
                 loss.backward()
-                
+
                 optimizer.step()
                 optimizer.zero_grad()
                 completed_step += 1
-                
-                
-                if completed_step % setup.log_every_n_step == 0 or completed_step == total_step:
+
+                if (
+                    completed_step % setup.log_every_n_step == 0
+                    or completed_step == total_step
+                ):
                     progress_tag = 100 * completed_step / total_step
                     logger.info(
                         f"[{progress_tag:.2f}%]\t epoch:{epoch}\t step:{completed_step}\t loss:{loss.item()}"
                     )
                     if self.writer is not None:
                         self.writer.add_scalar("loss", loss.item(), completed_step)
-                
+
                 if completed_step % setup.save_ckpt_n_step == 0:
                     psnr = self.eval(is_training=True, global_step=completed_step)
                     self.saver.save_model(psnr)
@@ -161,7 +168,7 @@ class Trainer:
         if self.writer is not None:
             self.writer.flush()
             self.writer.close()
-    
+
     def eval(self, is_training=False, global_step=0):
         datacfg = self.config.data
         dataset = DataSetSID(
@@ -169,21 +176,23 @@ class Trainer:
             file=Path(datacfg.data_root).joinpath("Sony_val_list.txt").absolute(),
         )
         dataloader = DataLoader(dataset, collate_fn=collate_fn)
-            
+
         eval_type = "validating" if is_training else "predicting"
         psnr_meter = utils.AverageValueMeter("psnr")
         ssim_meter = utils.AverageValueMeter("ssim")
-        
+
         with torch.no_grad():
             image_saved = False
             for _, data in enumerate(tqdm(dataloader, desc=eval_type)):
-                
-                input, labels = self.load_data(data[0], datacfg, transfroms=None, fixed=True)
+
+                input, labels = self.load_data(
+                    data[0], datacfg, transfroms=None, fixed=True
+                )
                 outputs = self.model(input)
-                
+
                 outputs = torch.clamp(outputs, 0, 1).cpu()
                 labels = labels.cpu()
-                
+
                 for output, label in zip(outputs, labels):
                     output = output.numpy().transpose(1, 2, 0) * 255.0
                     label = label.numpy().transpose(1, 2, 0) * 255.0
@@ -195,7 +204,7 @@ class Trainer:
                         gaussian_weights=True,
                         use_sample_covariance=False,
                         multichannel=True,
-                    ) 
+                    )
                     psnr_meter.add(psnr, 1)
                     ssim_meter.add(ssim, 1)
                     if is_training and not image_saved:
@@ -213,10 +222,6 @@ class Trainer:
                 f"{eval_type} finished! mean psnr = {psnr_meter.mean}, mean ssim = {ssim_meter.mean}"
             )
             if is_training and self.writer is not None:
-                self.writer.add_scalar(
-                    psnr_meter.name, psnr_meter.mean, global_step
-                )
-                self.writer.add_scalar(
-                    ssim_meter.name, ssim_meter.mean, global_step
-                )
+                self.writer.add_scalar(psnr_meter.name, psnr_meter.mean, global_step)
+                self.writer.add_scalar(ssim_meter.name, ssim_meter.mean, global_step)
             return psnr_meter.mean
